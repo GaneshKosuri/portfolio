db12a8ae5f5df906ef6fe39cf818c06ce10010a7
Ganesh Kosuri <ganeshkosuri9656@gmail.com>
2022-07-25 18:51:32 +0530

    update resume

--- src/App.js
+++ src/App.js
@@ -12,10 +12,6 @@ function App() {
 
   const { theme } = useContext(ThemeContext);
 
-  console.log("%cDEVELOPER PORTFOLIO", `color:${theme.primary}; font-size:50px`);
-  console.log("%chttps://github.com/hhhrrrttt222111/developer-portfolio", `color:${theme.tertiary}; font-size:20px`);
-  // console.log = console.warn = console.error = () => {};
-
   return (
     <div className="app">
       <Router>
@@ -24,8 +20,9 @@ function App() {
           <Route path="/" exact component={Main} />
           {/* <Route path="/blog" exact component={BlogPage} /> */}
           {/* <Route path="/projects" exact component={ProjectPage} /> */}
+          <Route path="/error" component={()=><h1>Sorry for inconvenience</h1>} />
 
-          <Redirect to="/" />
+          <Redirect to="/error"  />
         </Switch>
       </Router>
       <BackToTop />
