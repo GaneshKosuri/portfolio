1de03736b08212151376815b1aa200352c3e2f58
Ganesh Kosuri <ganeshkosuri9656@gmail.com>
2022-07-25 19:25:04 +0530

    update favicons and meta data

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -2,5 +2,5 @@ export const aboutData = {
     title: "Who I am",
     description1: "My name's Ganesh Kosuri. I'm a web designer and developer based in Hyderabad, India.",
     description2: "During the day I work as a web developer at PinScale Technologies private limited and in the evening I lean new tech stacks like Graphql. I spent my leisure hours on watching tech news, builing small pocs for the stack that I have been learning. Right now I'm also trying a hand at NextJS and Flutter. I love to learn and explore new arenas in Tech.",
-    image: 2
+    image: 1
 }
